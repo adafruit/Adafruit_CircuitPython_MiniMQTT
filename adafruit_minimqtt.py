6863435b8415e2d0c34ebb8446adc3a0b8f8babc
brentru <robots199Wme.com>
2019-06-27 13:15:36 -0400

    add init, starting connect...

--- adafruit_minimqtt.py
+++ adafruit_minimqtt.py
@@ -2,6 +2,9 @@
 #
 # Copyright (c) 2019 Brent Rubell for Adafruit Industries
 #
+# Original Work Copyright (c) 2016 Paul Sokolovsky, uMQTT
+# Modified Work Copyright (c) 2019 Bradley Beach, esp32spi_mqtt
+# 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -31,11 +34,6 @@ MQTT client library for CircuitPython
 Implementation Notes
 --------------------
 
-**Hardware:**
-
-.. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
-   inline format: "* `Link Text <url>`_"
-
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
@@ -43,11 +41,49 @@ Implementation Notes
 
 .. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies based on the library's use of either.
 
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+* Adafruit's ESP32SPI library: https://github.com/adafruit/Adafruit_CircuitPython_ESP32SPI/
 """
-
-# imports
+import time
+import struct
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MiniMQTT.git"
+
+
+class MQTT:
+    """
+    MQTT Client for CircuitPython.
+    """
+    def __init__(self, esp, socket, wifimanager, client_id, server_address, port=1883, user=None,
+                    password = None, is_ssl=False):
+        self._esp = esp
+        self._socket = socket
+        self._wifi_manager = wifimanager
+        if port == 0 and ssl:
+            port = 8883
+        else:
+            port = 1883
+        self.port = port
+        self.user = user
+        self._pass = password
+        self._client_id = client_id
+        self.server = server_address
+        self._is_ssl = is_ssl
+        self.packet_id = 0
+        self._keep_alive = 0
+        self._cb = None
+        self._lw_topic = None
+        self._lw_msg = None
+        self._lw_retain = False
+    
+    def connect(self, clean_session=True):
+        """Initiates connection with the MQTT Broker.
+        :param bool clean_session: Establishes a persistent session
+            with the broker. Defaults to a non-persistent session.
+        """
+        # get a socket
+        if self._esp:
+            self._socket.set_interface(self._esp)
+
+
+
