13010a4d3eaec1ba85c1a6c89dd04ed4a6feb838
brentru <robots199@me.com>
2019-07-02 18:11:42 -0400

    add larger example from pyportal

--- examples/minimqtt_simpletest.py
+++ examples/minimqtt_simpletest.py
@@ -5,7 +5,6 @@ from digitalio import DigitalInOut
 import neopixel
 from adafruit_esp32spi import adafruit_esp32spi
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
-from adafruit_esp32spi import adafruit_esp32spi_wifimanager
 
 from adafruit_minimqtt import MQTT
 
@@ -42,12 +41,9 @@ status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2) # Uncomment 
 # BLUE_LED = PWMOut.PWMOut(esp, 25)
 # status_light = adafruit_rgbled.RGBLED(RED_LED, BLUE_LED, GREEN_LED)
 
-# Create a WiFiManager
-wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
-
 # Instanciate a MQTT Client
-mqtt_client = MQTT(esp, socket, wifi, secrets['aio_url'],
-                    user=secrets['aio_user'], password=secrets['aio_password'],
+mqtt_client = MQTT(esp, socket, secrets['aio_url'],
+                    username=secrets['aio_user'], password=secrets['aio_password'],
                     is_ssl = True)
 
 print("Connecting to AP...")
@@ -62,16 +58,47 @@ print("My IP address is", esp.pretty_ip(esp.ip_address))
 
 print('Connecting to {0}:{1}...'.format(mqtt_client.server, mqtt_client.port))
 
+
+# ACK Callbacks for testing
+def connect(client, userdata, flags, rc):
+    """.connect() called"""
+    print('Connected to MQTT Broker!')
+    print('Flags: {0}\n RC: {1}'.format(flags, rc))
+
+def disconnect(client, userdata, rc):
+    """.disconnect() called"""
+    print('Disconnected from MQTT Broker!')
+    print('RC: {0}\n'.format(rc))
+
+def subscribe(client, userdata, qos):
+    """.subscribe() called"""
+    print('Client successfully subscribed to feed with a QOS of %d'%qos)
+
+def publish(client, userdata, pid):
+    """.publish() called"""
+    print('Client successfully published to feed with a PID of %d'%pid)
+
+# Set the user_data to a generated client_id
+mqtt_client.user_data = mqtt_client._client_id
+
+# define callbacks
+mqtt_client.on_connect = connect
+mqtt_client.on_subscribe = subscribe
+mqtt_client.on_publish = publish
+mqtt_client.on_disconnect = disconnect
+
 # Connect MQTT Client
+print('connecting...')
 mqtt_client.connect()
 
-# Subscribe to MQTT feed
-print('Subscribing to feed')
+# Subscribe to feed
+print('subscribing...')
 mqtt_client.subscribe('brubell/feeds/temperature')
 
-while True:
-    print('Publishing msg to feed')
-    mqtt_client.publish('brubell/feeds/temperature', "cat")
-    print('Listening for msg...')
-    mqtt_client.wait_msg()
-    time.sleep(5)
+print('publishing...')
+mqtt_client.publish('brubell/feeds/temperature', 50)
+
+# Disconnect from MQTT Client
+print('disconnecting...')
+mqtt_client.disconnect()
+
