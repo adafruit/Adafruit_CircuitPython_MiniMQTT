ffd87eb88c548e2661bac1cb30ce4dfa0b6890f1
brentru <robots199@me.com>
2019-07-05 12:43:15 -0400

    consolidate some tests, fixup bugs

--- examples/test_mqtt_client.py
+++ examples/test_mqtt_client.py
@@ -30,13 +30,12 @@ status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2) # Uncomment 
 
 
 """
-Generic Unittest Assertions
+Generic Unittest-like Assertions
 """
 #pylint: disable=keyword-arg-before-vararg
 def assertAlmostEqual(x, y, places=None, msg=''):
     """Raises an AssertionError if two float values are not equal.
-    (from https://github.com/micropython/micropython-lib/blob/master/unittest/unittest.py)
-    """
+    (from https://github.com/micropython/micropython-lib/blob/master/unittest/unittest.py)."""
     if x == y:
         return
     if places is None:
@@ -48,9 +47,8 @@ def assertAlmostEqual(x, y, places=None, msg=''):
     assert False, msg
 
 def assertRaises(exc,func=None, *args, **kwargs):
-    """Raises based on context.
-    (from https://github.com/micropython/micropython-lib/blob/master/unittest/unittest.py)
-    """
+    """Raises based on exception context.
+    (from https://github.com/micropython/micropython-lib/blob/master/unittest/unittest.py)"""
     try:
         func(*args, **kwargs)
         assert False, "%r not raised" % exc
@@ -60,79 +58,78 @@ def assertRaises(exc,func=None, *args, **kwargs):
         raise
 
 def assertIsNone(x):
-    """Raises an AssertionError if x is None.
-    """
+    """Raises an AssertionError if x is None."""
     if x is None:
         raise AssertionError('%r is None'%x)
 
 def assertEqual(val_1, val_2):
-    """Raises an AssertionError if the two specified values are not equal.
-    """
+    """Raises an AssertionError if the two specified values are not equal."""
     if val_1 != val_2:
         raise AssertionError('Values are not equal:', val_1, val_2)
 
-# Default MiniMQTT Callback Message Handlers
-def connect(client, userdata, flags, rc):
-    """.connect() called"""
-    print('Connected to MQTT Broker!')
-    print('Flags: {0}\n RC: {1}'.format(flags, rc))
-
-def disconnect(client, userdata, rc):
-    """.disconnect() called"""
-    print('Disconnected from MQTT Broker!')
-    print('RC: {0}\n'.format(rc))
-
-def subscribe(client, userdata, qos):
-    """.subscribe() called"""
-    print('Client successfully subscribed to feed with a QOS of %d'%qos)
-
-def publish(client, userdata, pid):
-    """.publish() called"""
-    print('Client successfully published to feed with a PID of %d'%pid)
-
-def unsubscribe(P1, P2):
-    # unsubscribe() called from the mqtt_client.
-    print('Client successfully unsubscribed from feed!')
-
 # MQTT Client Tests
-def test_create_insecure_mqtt_client_esp32spi():
+def test_mqtt_create_client_esp32spi():
     """Creates an insecure MQTT client using an ESP32SPI socket connection."""
     mqtt_client = MQTT(esp, socket, secrets['aio_url'],
                         username=secrets['aio_user'], password=secrets['aio_password'],
                         is_ssl=False)
+    mqtt_client.logging('DEBUG')
     assertEqual(mqtt_client.port, 1883)
 
-def test_create_secure_mqtt_client_esp32spi():
+def test_mqtts_create_client_esp32spi():
     """Creates a secure MQTT client using an ESP32SPI socket connection."""
     mqtt_client = MQTT(esp, socket, secrets['aio_url'],
                         username=secrets['aio_user'], password=secrets['aio_password'],
                         is_ssl=True)
+    mqtt_client.logging('DEBUG')
     assertEqual(mqtt_client.port, 8883)
 
-def test_connect_mqtts_esp32spi():
-    """Securely connects to a MQTT broker using an ESP32SPI_Socket connection."""
+def test_mqtts_connect_disconnect_esp32spi():
+    """Creates a MQTTS client, connects, and attempts a disconnection."""
     mqtt_client = MQTT(esp, socket, secrets['aio_url'],
                     username=secrets['aio_user'], password=secrets['aio_password'],
                     is_ssl=True)
+    mqtt_client.logging('DEBUG')
     mqtt_client.connect()
     assertEqual(mqtt_client._is_connected, True)
+    mqtt_client.disconnect()
+    assertEqual(mqtt_client._is_connected, False)
 
-def test_connect_mqtt_esp32spi():
-    """Inesecurely connects to a MQTT broker using an ESP32SPI_Socket connection."""
+def test_sub_pub():
+    """Creates a MQTTS client, connects, subscribes, publishes, and checks data
+    received from broker matches data sent by client"""
     mqtt_client = MQTT(esp, socket, secrets['aio_url'],
                     username=secrets['aio_user'], password=secrets['aio_password'],
-                    is_ssl=False)
+                    is_ssl=True)
+    mqtt_client.logging('DEBUG')
+    # Callback responses
+    callback_msgs = []
+    def on_message(client, topic, msg):
+        callback_msgs.append([topic, msg])
+        print(callback_msgs)
     mqtt_client.connect()
     assertEqual(mqtt_client._is_connected, True)
+    mqtt_client.subscribe('brubell/feeds/testfeed', on_message)
+    mqtt_client.publish('brubell/feeds/testfeed', 42)
+    start_timer = time.monotonic()
+    while len(callback_msgs) == 0 and (time.monotonic() - start_timer < 30):
+        mqtt_client.wait_for_msg()
+    # check message with payload sent has been RXd properly
+    print(callback_msgs)
 
 
 # Timeout between tests, in seconds. This value depends on the timeout of your MQTT broker.
 TEST_TIMEOUT = 1
 
-# Tests methods
-tests = [test_create_insecure_mqtt_client_esp32spi, test_create_secure_mqtt_client_esp32spi,
-            test_connect_mqtts_esp32spi, test_connect_mqtt_esp32spi]
+# Connection/Client Tests
+conn_tests = [test_mqtt_create_client_esp32spi, test_mqtts_create_client_esp32spi,
+                test_mqtts_connect_disconnect_esp32spi]
 
+# PUB/SUB API Tests
+pub_sub_tests = [test_sub_pub]
+
+# The test routine runs the following test(s):
+tests = pub_sub_tests
 
 # Establish ESP32SPI connection
 print("Connecting to AP...")
@@ -144,8 +141,7 @@ while not esp.is_connected:
         continue
 print("Connected to", str(esp.ssid, 'utf-8'), "\tRSSI:", esp.rssi)
 
-# Test harness
-
+## Test Routine ##
 start_time = time.monotonic()
 for i in enumerate(tests):
     print('Running test: ', i)
