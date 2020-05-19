8d53b311a4245e38bf6388b66a6218378ecdb325
brentru <robots199@me.com>
2020-05-19 16:44:48 -0400

    bump baud to 9600 for FONA, disable encryption for fona

--- examples/minimqtt_simpletest_cellular.py
+++ examples/minimqtt_simpletest_cellular.py
@@ -17,10 +17,8 @@ except ImportError:
     print("Cellular secrets are kept in secrets.py, please add them there!")
     raise
 
-# Create a serial connection for the FONA connection using 4800 baud.
-# These are the defaults you should use for the FONA Shield.
-# For other boards set RX = GPS module TX, and TX = GPS module RX pins.
-uart = busio.UART(board.TX, board.RX, baudrate=4800)
+# Create a serial connection for the FONA connection
+uart = busio.UART(board.TX, board.RX)
 rst = digitalio.DigitalInOut(board.D4)
 # Initialize FONA
 fona = FONA(uart, rst)
@@ -86,7 +84,10 @@ MQTT.set_socket(socket, fona)
 
 # Set up a MiniMQTT Client
 client = MQTT.MQTT(
-    broker=secrets["broker"], username=secrets["user"], password=secrets["pass"]
+    broker=secrets["broker"],
+    username=secrets["user"],
+    password=secrets["pass"],
+    is_ssl=False,
 )
 
 # Connect callback handlers to client
