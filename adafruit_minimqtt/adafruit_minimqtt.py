8f5749b9662e8491e978f991c684a010620595fb
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_minimqtt/adafruit_minimqtt.py
+++ adafruit_minimqtt/adafruit_minimqtt.py
@@ -32,7 +32,7 @@ from random import randint
 from micropython import const
 from .matcher import MQTTMatcher
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MiniMQTT.git"
 
 # Client-specific variables
