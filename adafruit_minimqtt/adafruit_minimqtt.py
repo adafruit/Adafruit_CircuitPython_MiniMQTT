a9ce58da3985c5bb71864e69aa770e8ebdc5f627
Tyeth Gundry <tyethgundry@googlemail.com>
2025-03-06 20:46:07 +0000

    format noqa line

--- adafruit_minimqtt/adafruit_minimqtt.py
+++ adafruit_minimqtt/adafruit_minimqtt.py
@@ -111,7 +111,7 @@ class NullLogger:
             setattr(NullLogger, log_level, self.nothing)
 
 
-class MQTT: # noqa: PLR0904  # too-many-public-methods
+class MQTT:  # noqa: PLR0904  # too-many-public-methods
     """MQTT Client for CircuitPython.
 
     :param str broker: MQTT Broker URL or IP Address.
